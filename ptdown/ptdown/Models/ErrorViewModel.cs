41870ab0c2e0af7c2b1b04938f7305eba74fa365
David Hartley <dhartley84@gmail.com>
2020-04-06 13:41:15 +1000

    Initial commit

---
+++ ptdown/ptdown/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ptdown.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
